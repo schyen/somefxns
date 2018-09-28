bf2f698d62a85ec1b0e42805406457bc44753a56
Sandi Yen <sandi.yen@gmail.com>
2018-09-28 10:55:58 -0400

    added import

--- R/importVictor.R
+++ R/importVictor.R
@@ -4,25 +4,15 @@
 #'
 #' @param file path and file name
 #'
-#' @return list of:
-#'     df - dataframe of wells, absorbance. is the first sheet of the victor file
-#'     plate - 2nd sheet of victor file
-#'     protocol - 3rd sheet of victor file
-#'     errors - 4th sheet of victor file
-#'     notes - 5th sheet of victor file
-#'
+#' \code{df} - dataframe of wells, absorbance. is the first sheet of the victor file;
+#' @import readxl
 
 importVictor <- function(file){
 
   sheet <- excel_sheets(file)
 
   # reading in read table
-  raw <- list('df' = as.data.frame(read_excel(file, sheet=sheet[1])) ,
-              'plate' = read_excel(file, sheet=sheet[2]),
-              'protocol' = read_excel(file, sheet=sheet[3]),
-              'errors' = read_excel(file, sheet=sheet[4]),
-              'notes' = read_excel(file, sheet=sheet[5])
-  )
+  raw <- as.data.frame(read_excel(file, sheet=sheet[1]))
   return(raw)
 
 }
